1f32c892d530ab1410ea53c987ca954861e1c258
Serhat Gultekin <104542830+MehmetSerhatGultekin@users.noreply.github.com>
2025-02-01 15:29:47 +0300

    end of project

--- MusicApp/SongsVC.swift
+++ MusicApp/SongsVC.swift
@@ -8,88 +8,104 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet var table: UITableView!
-    
-    var songs = [Song] ()
+
+    var songs = [Song]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSongs()
         table.delegate = self
         table.dataSource = self
     }
-    
+
     func configureSongs() {
-        songs.append(Song(name: "Blood Theme",
-                          albumName: "Dexter",
-                          artistName: "Show",
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
                           imageName: "cover1",
                           trackName: "song1"))
-        
-        songs.append(Song(name: "NKBİ",
-                          albumName: "Video",
-                          artistName: "Güneş",
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
                           imageName: "cover2",
                           trackName: "song2"))
-        
-        songs.append(Song(name: "Falling Down",
-                          albumName: "jinji",
-                          artistName: "lil peep & xxxtentaction",
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
                           imageName: "cover3",
                           trackName: "song3"))
-        
-        songs.append(Song(name: "Blood Theme",
-                          albumName: "Dexter",
-                          artistName: "Show",
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
                           imageName: "cover1",
                           trackName: "song1"))
-        
-        songs.append(Song(name: "NKBİ",
-                          albumName: "Video",
-                          artistName: "Güneş",
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
                           imageName: "cover2",
                           trackName: "song2"))
-        
-        songs.append(Song(name: "Falling Down",
-                          albumName: "jinji",
-                          artistName: "lil peep & xxxtentaction",
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
+                          imageName: "cover3",
+                          trackName: "song3"))
+        songs.append(Song(name: "Background music",
+                          albumName: "123 Other",
+                          artistName: "Rnado",
+                          imageName: "cover1",
+                          trackName: "song1"))
+        songs.append(Song(name: "Havana",
+                          albumName: "Havana album",
+                          artistName: "Camilla Cabello",
+                          imageName: "cover2",
+                          trackName: "song2"))
+        songs.append(Song(name: "Viva La Vida",
+                          albumName: "123 Something",
+                          artistName: "Coldplay",
                           imageName: "cover3",
                           trackName: "song3"))
     }
-    
+
+    // Table
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songs.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         let song = songs[indexPath.row]
-        
+        // configure
         cell.textLabel?.text = song.name
-        cell .detailTextLabel?.text = song.albumName
-        cell.accessoryType = .disclosureIndicator // açıklama göstergesi eklemek için kullanıldı
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
         cell.imageView?.image = UIImage(named: song.imageName)
-        cell.textLabel?.font = UIFont(name: "Calibri-Bold", size: 18)
-        cell.textLabel?.font = UIFont(name: "Calibri", size: 16)
-        
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100 // row boyutu
+        return 100
     }
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
+
+        // present the player
         let position = indexPath.row
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") as? PlayerVC else {
+        guard let vc = storyboard?.instantiateViewController(identifier: "player") as? PlayerViewController else {
             return
         }
         vc.songs = songs
         vc.position = position
         present(vc, animated: true)
     }
-    
+
+
 }
 
 struct Song {
